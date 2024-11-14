2859712d07462ed82ace1767e40c5a7fdf15ce2d
Barış Okan Fındık <barisokanfindik@gmail.com>
2024-11-14 04:10:06 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Redirecting</title>
+<script type="text/javascript">
+  if (window.location.host === 'siteniz.tumblr.com') {
+    window.location.replace(window.location.href.replace('siteniz.tumblr.com', 'siteniz.com'));
+  }
+</script>
+</head>
+<body>
+  <a href="https://siteniz.com">Yeni URL: Eski URL artık Yeni URL’ye yönlendirilmiştir.</a>
+</body>
+</html>
